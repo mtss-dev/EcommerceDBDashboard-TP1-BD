34c0c64a6b7c793eb10c9cfb4ea50d0096068cdb
mtss-dev <matheus.silva@icomp.ufam.edu.br>
2023-04-09 17:10:59 -0400

    Insert funcionando na tabela Product e Similar_Products

--- scripts/insert copy.py
+++ scripts/insert copy.py
@@ -1,68 +1,31 @@
+#!/usr/bin/python
 import psycopg2
 from config import config
 
 
-def insert_vendor(vendor_name):
-    """ insert a new vendor into the vendors table """
-    sql = """INSERT INTO vendors(vendor_name)
-             VALUES(%s) RETURNING vendor_id;"""
+def add_part(part_name, vendor_list):
+    # statement for inserting a new row into the parts table
+    insert_part = "INSERT INTO parts(part_name) VALUES(%s) RETURNING part_id;"
+    # statement for inserting a new row into the vendor_parts table
+    assign_vendor = "INSERT INTO vendor_parts(vendor_id,part_id) VALUES(%s,%s)"
+
     conn = None
-    vendor_id = None
     try:
-        # read database configuration
         params = config()
-        # connect to the PostgreSQL database
         conn = psycopg2.connect(**params)
-        # create a new cursor
         cur = conn.cursor()
-        # execute the INSERT statement
-        cur.execute(sql, (vendor_name,))
-        # get the generated id back
-        vendor_id = cur.fetchone()[0]
-        # commit the changes to the database
+        # insert a new part
+        cur.execute(insert_part, (part_name,))
+        # get the part id
+        part_id = cur.fetchone()[0]
+        # assign parts provided by vendors
+        for vendor_id in vendor_list:
+            cur.execute(assign_vendor, (vendor_id, part_id))
+
+        # commit changes
         conn.commit()
-        # close communication with the database
-        cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if conn is not None:
             conn.close()
-
-    return vendor_id
-
-def insert_vendor_list(vendor_list):
-    """ insert multiple vendors into the vendors table  """
-    sql = "INSERT INTO vendors(vendor_name) VALUES(%s)"
-    conn = None
-    try:
-        # read database configuration
-        params = config()
-        # connect to the PostgreSQL database
-        conn = psycopg2.connect(**params)
-        # create a new cursor
-        cur = conn.cursor()
-        # execute the INSERT statement
-        cur.executemany(sql,vendor_list)
-        # commit the changes to the database
-        conn.commit()
-        # close communication with the database
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-
-if __name__ == '__main__':
-    # insert one vendor
-    insert_vendor("3M Co.")
-    # insert multiple vendors
-    insert_vendor_list([
-        ('AKM Semiconductor Inc.',),
-        ('Asahi Glass Co Ltd.',),
-        ('Daikin Industries Ltd.',),
-        ('Dynacast International Inc.',),
-        ('Foster Electric Co. Ltd.',),
-        ('Murata Manufacturing Co. Ltd.',)
-    ])
--- scripts/insert.py
+++ scripts/insert.py
@@ -1,6 +1,7 @@
 import psycopg2
 from config import config
 
+#A função abaixo insere dados somente na tabela product
 def insert_product_list(product_list):
     """ insert multiple vendors into the vendors table  """
     sql = "INSERT INTO product(asin,title,product_group,sales_rank) VALUES(%s,%s,%s,%s)"
@@ -25,7 +26,38 @@ def insert_product_list(product_list):
             conn.close()
 
 
+#A função abaixo insere dados na tabela product e similar_products
+def add_product(asin,title,product_group,sales_rank, assins_list):
+    # statement for inserting a new row into the parts table
+    insert_product = "INSERT INTO product(asin,title,product_group,sales_rank) VALUES(%s,%s,%s,%s) RETURNING id;"
+    # statement for inserting a new row into the vendor_parts table
+    assing_similar = "INSERT INTO similar_products(asin_similar,product_id) VALUES(%s,%s)"
+
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # insert a new part
+        cur.execute(insert_product, (asin,title,product_group,sales_rank,))
+        # get the part id
+        id = cur.fetchone()[0]
+        # assign parts provided by vendors
+        for asin_similar in assins_list:
+            cur.execute(assing_similar, (asin_similar, id))
+
+        # commit changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+
+
 if __name__ == '__main__':
-    insert_product_list([
-        ('0827229534','Patterns of Preaching: A Sermon Sampler','Book','396585')
-    ])
+    #insert_product_list([
+        #('0827229534','Patterns of Preaching: A Sermon Sampler','Book','396585')])
+    add_product('0827229534','Patterns of Preaching: A Sermon Sampler','Book','396585', ('0804215715','156101074X','0687023955','0687074231','082721619X'))
