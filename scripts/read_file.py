1b4a107105ce4d1dd837bb9192ebb9b61e4045b3
VinnyHardth <vinicius.fonseca@icomp.ufam.edu.br>
2023-04-14 18:31:56 -0400

    atualização read_file

--- scripts/read_file.py
+++ scripts/read_file.py
@@ -1,29 +1,28 @@
-def read_asin(line):
-    if 'ASIN' in line:
-        asin = line.split("ASIN: ")[1]
-    return asin
 
-def read_title(line):
-    if 'title' in line:
-        title = line.split("title: ")[1]
-    return title
+def populate():
 
-def read_group(line):
-    if 'group' in line:
-        group = line.split("group: ")[1]
-    return group
+    file = open('teste.txt', 'r')
 
-def read_salesrank(line):
-    if 'salesrank' in line:
-        salesrank = line.split("salesrank: ")[1]
-    return salesrank
-
-def build_product(file):
-    while True:
-        line = file.readline()
-        asin = read_asin(file.readline())
-
+    for line in file:
+        if 'discontinued' in line:
+            asin = title = group = salesrank = ''
+            continue
+        if 'ASIN' in line:
+            asin = line.split("ASIN: ")[1]
+            continue
+        if 'title' in line:
+            title = line.split("title: ")[1]
+            continue
+        if 'group' in line:
+            group = line.split("group: ")[1]
+            continue
+        if 'salesrank' in line:
+            salesrank = line.split("salesrank: ")[1]
+            continue
+        if 'categories' in line:
+            print(asin, title, group, salesrank)
+            continue
 
 if __name__ == '__main__':
     file = open("teste.txt", "r")
-    print(build_product(file))
+    populate()
