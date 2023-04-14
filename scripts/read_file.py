72eb588f2e7e7266da5a6135ee99128298695c48
VinnyHardth <vinicius.fonseca@icomp.ufam.edu.br>
2023-04-14 19:14:08 -0400

    atualização 0.3 ajeitando enter

--- scripts/read_file.py
+++ scripts/read_file.py
@@ -9,21 +9,23 @@ def populate():
             asin = title = group = salesrank = ''
             continue
         if 'ASIN' in line:
-            asin = line.split("ASIN: ")[1]
+            asin = line.split("ASIN: ")[1].strip()
             continue
         if 'title' in line:
-            title = line.split("title: ")[1]
+            title = line.split("title: ")[1].strip()
             continue
         if 'group' in line:
-            group = line.split("group: ")[1]
+            group = line.split("group: ")[1].strip()
             continue
         if 'salesrank' in line:
-            salesrank = line.split("salesrank: ")[1]
+            salesrank = line.split("salesrank: ")[1].strip()
             continue
         if 'similar' in line:
             similar = line.split()[2:]
+            for i in range(len(similar)):
+                similar[i] = similar[i].strip()
         if 'categories' in line:
-            add_product(asin, title, group, salesrank,[])
+            add_product(asin, title, group, salesrank,similar)
             continue
 
 if __name__ == '__main__':
